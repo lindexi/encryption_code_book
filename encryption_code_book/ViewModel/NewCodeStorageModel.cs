1197545f2a2dbe32b519ea4003bba697834c798f
lindexi_gd <lindexi_gd@163.com>
2016-11-03 21:36:28 +0800

    新建密码本

--- encryption_code_book/ViewModel/NewCodeStorageModel.cs
+++ encryption_code_book/ViewModel/NewCodeStorageModel.cs
@@ -3,10 +3,92 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Storage;
 
 namespace encryption_code_book.ViewModel
 {
-    public class NewCodeStorageModel
+    public class NewCodeStorageModel:NotifyProperty
     {
+        public NewCodeStorageModel()
+        {
+
+        }
+
+        private string _name;
+
+        public string Name
+        {
+            set
+            {
+                _name = value;
+                OnPropertyChanged();
+            }
+            get
+            {
+                return _name;
+            }
+        }
+
+        private string _comfirmKey;
+       
+
+        public string Str
+        {
+            set
+            {
+                _str = value;
+                OnPropertyChanged();
+            }
+            get
+            {
+                return _str;
+            }
+        }
+
+        public string ComfirmKey
+        {
+            set
+            {
+                _comfirmKey = value;
+                OnPropertyChanged();
+            }
+            get
+            {
+                return _comfirmKey;
+            }
+        }
+
+        public StorageFolder EncryCodeFolder
+        {
+            set
+            {
+                _encryCodeFolder = value;
+                OnPropertyChanged();
+            }
+            get
+            {
+                return _encryCodeFolder;
+            }
+        }
+
+        private StorageFolder _encryCodeFolder;
+
+
+        private string _str;
+
+        public void NewCodeStorage()
+        {
+            
+        }
+
+        public void NewEncryCodeFolder()
+        {
+            
+        }
+
+        public void NavigateEncryCodeStoragePage()
+        {
+            
+        }
     }
 }
