ad70d816a1f382708764326a495f0b95bdafdc1b
lindexi_gd <lindexi_gd@163.com>
2016-11-29 16:25:02 +0800

    测试建立网站密码

---
+++ cencry_密码本/NewKeyUrl.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cencry_密码本
+{
+    class NewKeyUrl
+    {
+    }
+}
--- cencry_密码本/string_encryption.cs
+++ cencry_密码本/string_encryption.cs
@@ -15,25 +15,25 @@ namespace cencry密码本
     public class string_encryption
     {
         [TestMethod]
-        public void encryption()
-        {
-            encryption_code_book.Model.string_encryption encry = new encryption_code_book.Model.string_encryption();
-            string str;
-            string key;
-            Random ran = new Random();
-            //随机密钥
-            key = ranstr(10, ran);
-            string temp;
-            for (int i = 0; i < 10; i++)
-            {
-                //随机字符串
-                str = ranstr(100, ran);
-                temp = encry.encryption(str, key);
-                Assert.AreEqual(str, encry.decryption(temp, key));
-            }
+        //public void encryption()
+        //{
+        //    encryption_code_book.Model.string_encryption encry = new encryption_code_book.Model.string_encryption();
+        //    string str;
+        //    string key;
+        //    Random ran = new Random();
+        //    //随机密钥
+        //    key = ranstr(10, ran);
+        //    string temp;
+        //    for (int i = 0; i < 10; i++)
+        //    {
+        //        //随机字符串
+        //        str = ranstr(100, ran);
+        //        temp = encry.encryption(str, key);
+        //        Assert.AreEqual(str, encry.decryption(temp, key));
+        //    }
 
-            //随机错误密码
-        }
+        //    //随机错误密码
+        //}
 
         private string ranstr(int count, Random ran)
         {
