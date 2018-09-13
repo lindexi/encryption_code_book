e98b76463a07515db934f923b8fd239cd293f351
lindexi_gd <lindexi_gd@163.com>
2018-09-13 21:27:27 +0800

    添加加密类

---
+++ EncryptionCodeBook/Model/LaculooTome.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptionCodeBook.Model
+{
+    class LaculooTome
+    {
+        /// <summary>
+        /// 加密
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="str"></param>
+        public string Encryption(string key, string str)
+        {
+            return str;
+        }
+
+        /// <summary>
+        /// 解密
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="str"></param>
+        public string Decryption(string key, string str)
+        {
+            return str;
+        }
+    }
+}
