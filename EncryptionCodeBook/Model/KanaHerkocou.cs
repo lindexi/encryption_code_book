d2ab3355bdfea89b8a7f2ac6fd49919b3614c714
lindexi_gd <lindexi_gd@163.com>
2018-09-13 21:43:40 +0800

    存放的文件和解密文件密码

---
+++ EncryptionCodeBook/Model/KanaHerkocou.cs
@@ -0,0 +1,15 @@
+namespace EncryptionCodeBook.Model
+{
+    class KanaHerkocou
+    {
+        /// <summary>
+        /// 对应的文件
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 解密文件的密码
+        /// </summary>
+        public string Key { get; set; }
+    }
+}
