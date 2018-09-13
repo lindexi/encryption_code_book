d4548e74d967e2ca0f2d5178a569056c810981b4
lindexi_gd <lindexi_gd@163.com>
2018-09-13 22:42:26 +0800

    修改加密

--- EncryptionCodeBook/Model/LaculooTome.cs
+++ EncryptionCodeBook/Model/LaculooTome.cs
@@ -1,8 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
+using Windows.Security.Cryptography;
+using Windows.Security.Cryptography.Core;
+using Windows.Storage.Streams;
+using Buffer = Windows.Storage.Streams.Buffer;
 
 namespace EncryptionCodeBook.Model
 {
@@ -13,9 +19,16 @@ namespace EncryptionCodeBook.Model
         /// </summary>
         /// <param name="key"></param>
         /// <param name="str"></param>
-        public string Encryption(string key, string str)
+        public byte[] Encryption(string key, string str)
         {
-            return str;
+            var jaypejapeazaGeakeleargairhem = StringToBuffer(key);
+
+            var algorithm = SymmetricKeyAlgorithmProvider.OpenAlgorithm(SymmetricAlgorithmNames.DesCbcPkcs7);
+            var lelergemiXoukirow = algorithm.CreateSymmetricKey(jaypejapeazaGeakeleargairhem);
+
+            var nownaitooPearrelayya = CryptographicEngine.Encrypt(lelergemiXoukirow, StringToBuffer(str), null);
+
+            return nownaitooPearrelayya.ToArray();
         }
 
         /// <summary>
@@ -23,9 +36,21 @@ namespace EncryptionCodeBook.Model
         /// </summary>
         /// <param name="key"></param>
         /// <param name="str"></param>
-        public string Decryption(string key, string str)
+        public string Decryption(string key, byte[] str)
         {
-            return str;
+            var jaypejapeazaGeakeleargairhem = StringToBuffer(key);
+
+            var algorithm = SymmetricKeyAlgorithmProvider.OpenAlgorithm(SymmetricAlgorithmNames.DesCbcPkcs7);
+            var lelergemiXoukirow = algorithm.CreateSymmetricKey(jaypejapeazaGeakeleargairhem);
+
+            var neatearSallwi = CryptographicEngine.Decrypt(lelergemiXoukirow, str.AsBuffer(), null);
+            return Encoding.UTF8.GetString(neatearSallwi.ToArray());
+        }
+
+
+        private IBuffer StringToBuffer(string str)
+        {
+            return CryptographicBuffer.ConvertStringToBinary(str, BinaryStringEncoding.Utf8);
         }
     }
 }
