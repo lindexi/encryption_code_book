af650dbf5db3383e1526aed50fc76d6cd6e83b3d
lindexi <lindexi_gd@163.com>
2024-11-30 10:53:03 +0800

    重新完成简单的加密算法

--- Src/EncryptionAlgorithm/BinaryEncryption_1_1_2.cs
+++ Src/EncryptionAlgorithm/BinaryEncryption_1_1_2.cs
@@ -51,6 +51,137 @@ namespace Lindexi.Src.EncryptionAlgorithm
             var rawDataSpan = new ByteSpan(data, dataStart, dataLength);
             var hashList = new BitArray(outputBuffer.Length);
             var context = new EncryptionContext(rawDataSpan, key, hashList, outputBuffer, bufferLength, random);
+            EncryptDataCore_1_1_2(context);
+        }
+
+        private static void EncryptDataCore_1_1_2(EncryptionContext context)
+        {
+            var dataLength = context.DataLength;
+            for (var i = 0; i < dataLength; i++)
+            {
+                // 密码位置
+                var hashData = GetPlace_1_1_2(context, i);
+
+                byte dataValue = context.GetData(i);
+
+                // 这里算法上存在缺陷，那就是让某个下标的元素的数据耦合了 Key 的数据，让其之间存在关联关系
+                // 用人话说就是 HashValue 这个下标是依靠 KeyValue 计算出来的，因此在知道总数是 1024 的情况下，即可推断 HashValue 下标的内容加的可能是多少的值。假定不考虑二圈的情况，只考虑一圈时，如果此时 HashValue 是 2 的值，密文里是 99 的值，那极有可能是 KeyValue 是 2 或 1026 的值，将其减去 2 则得到密码是 97 即 'a' 的值。如此即可大概破解部分内容，甚至为后续更进一步推断提供更多信息
+                // 原设计里面，这里应该加的是计算出 HashValue 的后一位密码
+                dataValue = (byte) (dataValue + hashData.KeyValue);
+
+                context.Buffer[hashData.HashValue] = dataValue;
+                context.HashList[hashData.HashValue] = true;
+            }
+
+            // 填充空白部分
+            for (var i = 0; i < context.HashList.Length; i++)
+            {
+                if (!context.HashList[i])
+                {
+                    context.Buffer[i] = (byte) context.Random.Next(byte.MinValue, byte.MaxValue);
+                    context.HashList[i] = true;
+                }
+            }
+        }
+
+        /// <summary>
+        ///     根据传入参数，获取第 <paramref name="index" /> 个坐标
+        /// </summary>
+        /// <returns></returns>
+        private static HashData GetPlace_1_1_2(EncryptionContext context,int index)
+        {
+            int bufferLength = context.BufferLength;
+
+            int[] key = context.Key;
+            var keyPlace = index % key.Length;
+
+            // 读取的密码位置，可以计算出字符位置
+            var keyValue = key[keyPlace];
+            BitArray hashList = context.HashList;
+
+            var hashValue = keyValue % bufferLength;
+            if (!hashList[hashValue])
+            {
+                // 第零次，首次命中，此时不应该使用密码本身，应该选用下一个密码，避免被此计算出来
+                return new HashData(hashValue, keyValue);
+            }
+
+            // 第一圈是使用密码自身
+            foreach (var _ in key)
+            {
+                keyPlace++;
+                keyPlace = keyPlace % key.Length;
+                keyValue += key[keyPlace] * index;
+                // 由于 key[keyPlace] * i 可能存在越界问题，如果越界就会计算出负数，这里需要重新修改为正数
+                keyValue = Math.Abs(keyValue);
+                hashValue = keyValue % bufferLength;
+
+                if (!hashList[hashValue])
+                {
+                    return new HashData(hashValue, keyValue);
+                }
+            }
+
+            // 第二圈是尝试加上明文本身
+            const int sizeOfInt = 4;
+            if (index > sizeOfInt)
+            {
+                // 为什么 j = i - 1 原因是如果是解密的过程，那么当前的明文依然未知
+                // 只能取已经解密过的明文来参加计算
+                for (var j = index - 1; j >= sizeOfInt; j -= sizeOfInt)
+                {
+                    var byte1 = ReadByte(context, j);
+                    var byte2 = ReadByte(context, j - 1);
+                    var byte3 = ReadByte(context, j - 2);
+                    var byte4 = ReadByte(context, j - 3);
+
+                    var n = (byte1 << (3 * 8))
+                            + (byte2 << (2 * 8))
+                            + (byte3 << (1 * 8))
+                            + byte4;
+                    n *= index;
+                    keyValue = keyValue ^ n;
+                    //if (keyValue < 0)
+                    //{
+                    //    keyValue *= -1;
+                    //}
+                    keyValue = Math.Abs(keyValue); // 使用 Math.Abs 会比判断小于零再乘以负一快一点，使用 Math.Abs 指令优化
+
+                    hashValue = keyValue % bufferLength;
+                    if (!hashList[hashValue])
+                    {
+                        return new HashData(hashValue, keyValue);
+                    }
+                }
+            }
+            else if (index > 0)
+            {
+                // 为什么 j = i - 1 原因是如果是解密的过程，那么当前的明文依然未知
+                for (var j = index - 1; j >= 0; j--)
+                {
+                    keyValue += ReadByte(context, j);
+                    hashValue = keyValue % bufferLength;
+
+                    if (!hashList[hashValue])
+                    {
+                        return new HashData(hashValue, keyValue);
+                    }
+                }
+            }
+
+            while (hashList[hashValue])
+            {
+                // 最后就是 +1 了
+                keyValue++;
+                hashValue = keyValue % bufferLength;
+            }
+
+            return new HashData(hashValue, keyValue);
+
+            static byte ReadByte(EncryptionContext context, int index)
+            {
+                return context.GetData(index);
+            }
         }
 
         private const int SizeofDataLengthInt = 4; // sizeof(int)
@@ -69,6 +200,9 @@ namespace Lindexi.Src.EncryptionAlgorithm
 
             private readonly ByteSpan _rawDataSpan;
             public int[] Key { get; }
+            /// <summary>
+            /// 和 buffer 构成哈希，用来决定 buffer 上的位置是否被写入值
+            /// </summary>
             public BitArray HashList { get; }
             public byte[] Buffer { get; }
             public int BufferLength { get; }
@@ -110,115 +244,6 @@ namespace Lindexi.Src.EncryptionAlgorithm
             public byte[] Data { get; }
             public int Start { get; }
             public int Length { get; }
-        }
-
-        /// <summary>
-        ///     根据传入参数，获取第 <paramref name="i" /> 个坐标
-        /// </summary>
-        /// <param name="hashList"></param>
-        /// <param name="key">密码</param>
-        /// <param name="bufferLength">缓存长度</param>
-        /// <param name="data">明文数据</param>
-        /// <param name="suffixData">后缀</param>
-        /// <param name="i"></param>
-        /// <returns></returns>
-        private static HashData GetPlace_1_1_2(BitArray hashList, int[] key, int bufferLength, byte[] data, byte[] suffixData,
-            int i)
-        {
-            var keyPlace = i % key.Length;
-
-            // 读取的密码位置，可以计算出字符位置
-            var keyValue = key[keyPlace];
-
-            var hashValue = keyValue % bufferLength;
-            if (!hashList[hashValue])
-            {
-                // 第零次，首次命中，此时不应该使用密码本身，应该选用下一个密码，避免被此计算出来
-                return new HashData(hashValue, keyValue);
-            }
-
-            // 第一圈是使用密码自身
-            foreach (var _ in key)
-            {
-                keyPlace++;
-                keyPlace = keyPlace % key.Length;
-                keyValue += key[keyPlace] * i;
-                // 由于 key[keyPlace] * i 可能存在越界问题，如果越界就会计算出负数，这里需要重新修改为正数
-                keyValue = Math.Abs(keyValue);
-                hashValue = keyValue % bufferLength;
-
-                if (!hashList[hashValue])
-                {
-                    return new HashData(hashValue, keyValue);
-                }
-            }
-
-            // 第二圈是尝试加上明文本身
-            const int sizeOfInt = 4;
-            if (i > sizeOfInt)
-            {
-                // 为什么 j = i - 1 原因是如果是解密的过程，那么当前的明文依然未知
-                // 只能取已经解密过的明文来参加计算
-                for (var j = i - 1; j >= sizeOfInt; j -= sizeOfInt)
-                {
-                    var byte1 = GetByte(data, suffixData, j);
-                    var byte2 = GetByte(data, suffixData, j - 1);
-                    var byte3 = GetByte(data, suffixData, j - 2);
-                    var byte4 = GetByte(data, suffixData, j - 3);
-
-                    var n = (byte1 << (3 * 8))
-                            + (byte2 << (2 * 8))
-                            + (byte3 << (1 * 8))
-                            + byte4;
-                    n *= i;
-                    keyValue = keyValue ^ n;
-                    //if (keyValue < 0)
-                    //{
-                    //    keyValue *= -1;
-                    //}
-                    keyValue = Math.Abs(keyValue); // 使用 Math.Abs 会比判断小于零再乘以负一快一点，使用 Math.Abs 指令优化
-
-                    hashValue = keyValue % bufferLength;
-                    if (!hashList[hashValue])
-                    {
-                        return new HashData(hashValue, keyValue);
-                    }
-                }
-            }
-            else if (i > 0)
-            {
-                // 为什么 j = i - 1 原因是如果是解密的过程，那么当前的明文依然未知
-                for (var j = i - 1; j >= 0; j--)
-                {
-                    keyValue += GetByte(data, suffixData, j);
-                    hashValue = keyValue % bufferLength;
-
-                    if (!hashList[hashValue])
-                    {
-                        return new HashData(hashValue, keyValue);
-                    }
-                }
-            }
-
-            while (hashList[hashValue])
-            {
-                // 最后就是 +1 了
-                keyValue++;
-                hashValue = keyValue % bufferLength;
-            }
-
-            return new HashData(hashValue, keyValue);
-
-            static byte GetByte(byte[] a, byte[] b, int n)
-            {
-                if (n < a.Length)
-                {
-                    return a[n];
-                }
-
-                var length = n - a.Length;
-                return b.Length > length ? b[length] : (byte) 0;
-            }
         }
     }
 }
